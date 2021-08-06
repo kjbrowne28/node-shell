d62a5e7830a424a15fabcfd6984b8d61034ce47c
Kevin Browne <kjbrowne28@gmail.com>
2021-08-06 15:47:50 -0400

    finished modules

--- bash.js
+++ bash.js
@@ -1,11 +1,24 @@
+const pwd = require("./pwd")
+const ls = require("./ls")
+
+
 process.stdout.write('prompt > ');
-// process.stdin.on('data', (data) => {
-//     const cmd = data.toString().trim();
-//     process.stdout.write('You typed: ' + cmd) 
-//     process.stdout.write('\nprompt > ') 
-// })
+// // process.stdin.on('data', (data) => {
+// //     const cmd = data.toString().trim();
+// //     process.stdout.write('You typed: ' + cmd) 
+// //     process.stdout.write('\nprompt > ') 
+// // })
 process.stdin.on('data', (data) => {
-    if (data.toString().toUpperCase().trim() === 'PWD') {
-        process.stdout.write(__dirname)
+    let command = data.toString().toLowerCase().trim()
+    if (command === 'pwd'){
+        pwd()
     }
+    else if (command === "ls"){
+        ls()
+    }
+    return
+    // if (data.toString().toUpperCase().trim() === 'PWD') {
+    //     pwd()
+    // } else 
 })
+
---
+++ pwd.js
@@ -0,0 +1,10 @@
+
+
+module.exports = () => {
+    // process.stdout.write('prompt > ');
+    // process.stdin.on('data', (data) => {
+    //     if (data.toString().toUpperCase().trim() === 'PWD') {
+            process.stdout.write(__dirname)
+    //     }
+    // })
+}
